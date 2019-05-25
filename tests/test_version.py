a849f7a9a473f9c52a9ebb30c920bb691d744bd8
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-05-25 14:50:03 +0200

    updated test

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,4 +2,4 @@ from extra_keras_utils.__version__ import __version__
 import re
 
 def test_version():
-    assert re.compile("\d+\.\d+\.\d+").match(__version__)
+    assert re.compile(r"\d+\.\d+\.\d+").match(__version__)
